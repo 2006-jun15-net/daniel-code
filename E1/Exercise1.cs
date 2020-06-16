0f7efb78284307628c8991676f66e241bc06dd00
Daniel Aasa <danielaasa42@gmail.com>
2020-06-16 14:26:43 -0700

    exercise 1

---
+++ E1/Exercise1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int x = 0; x<=7; x++)
+           {
+               for(int y = 0;y<=(7-x); y++)
+               {
+                Console.Write(" ");
+               }
+               for (int z = 7; z>=(7-x); z--)
+               {
+                   Console.Write("#");
+               }
+               Console.WriteLine("");
+           }
+        }
+    }
+}
