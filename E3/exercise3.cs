c0feb9f783e3c13e0a6948e207b58d3a2925eee9
Daniel Aasa <danielaasa42@gmail.com>
2020-06-16 14:57:20 -0700

    exercise 3

---
+++ E3/exercise3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string userInput = Console.ReadLine();
+             int number = int.Parse(userInput);
+             while (number != 1)
+             {
+                if ((number%2) == 0)
+                {
+                    number = number/2;
+                }
+                else
+                {
+                    number = (number * 3) + 1;
+                }
+                Console.WriteLine(number);
+             }
+        }
+    }
+}
