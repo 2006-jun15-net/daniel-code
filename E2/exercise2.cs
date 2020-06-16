ef68aae4967747419602cf3d9cded0bdb24996ee
Daniel Aasa <danielaasa42@gmail.com>
2020-06-16 14:42:34 -0700

    exercise 2

---
+++ E2/exercise2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            int num = int.Parse(userInput);
+            num = num -1;
+         for (int x = 0; x<=num; x++)
+           {
+               for(int y = 0;y<=(num-x); y++)
+               {
+                Console.Write(" ");
+               }
+               for (int z = num; z>=(num-x); z--)
+               {
+                   Console.Write("#");
+               }
+               Console.WriteLine("");
+           }
+        }
+    }
+}
