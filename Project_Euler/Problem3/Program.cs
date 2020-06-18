576bd906cd445d2d9427bff53e17bebbc35b4109
Daniel Aasa <danielaasa42@gmail.com>
2020-06-18 14:48:40 -0700

    Problem 3 complete

---
+++ Project_Euler/Problem3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 600851475143;
+            int count = 1;
+            double max = Math.Sqrt(x);
+            max = Math.Round(max);
+            int answer = 0;
+
+            while (count<max)
+            {
+                if(( x % count ) == 0)
+                {
+                    answer = count;
+                }
+
+                count = count +2;
+            }
+
+            count =1;
+            int Prime = 0;
+
+            while (count<answer)
+            {
+                if(( answer % count ) == 0)
+                {
+                    Prime = count;
+                }
+
+                count = count +2;
+            }
+
+            Console.WriteLine("The answer is: " + Prime);
+        }
+    }
+}
