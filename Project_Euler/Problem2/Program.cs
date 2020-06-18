4f15f93444b9f4974c0d3a1f5c1d6c3f2f6eeb6b
Daniel Aasa <danielaasa42@gmail.com>
2020-06-18 12:44:02 -0700

    Problem2 complete

---
+++ Project_Euler/Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int past1 = 1;
+            int past2 = 0;
+            int answer = 0;
+            int current = 0;
+
+            while ( current <= 4000000 )
+            {
+                current = past2 + past1;
+
+                if ( current % 2 == 0)
+                {
+                    answer = answer + current;
+                }
+                past2 = past1;
+                past1 = current;
+            }
+            Console.WriteLine("The answer is: " + answer);
+        }
+    }
+}
