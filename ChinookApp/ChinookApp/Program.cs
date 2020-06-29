5f7a4322fd5f56a5553f4cdb785cece49fd4de7b
Daniel Aasa <danielaasa42@gmail.com>
2020-06-29 10:24:51 -0700

    sql week challenge

--- ChinookApp/ChinookApp/Program.cs
+++ ChinookApp/ChinookApp/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using ChinookApp.DataConnect.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
 
 namespace ChinookApp
 {
     class Program
     {
+        
+            public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.(); });
+                                                                //place file path for text file
+            String connectionString = System.IO.File.ReadAllText("C:/Users/james/Desktop/Revature/DBconnect.txt");
+            DbContextOptions<ChinookContext> options = new DbContextOptionsBuilder<ChinookContext>()
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlServer(connectionString)
+                .Options;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DisplayData();
+
+            Console.WriteLine("\nAdding some data...");
+            AddSomeDataFromUserInput();
+
+            DisplayData();
+
+            Console.WriteLine("\nUpdating some data...");
+            UpdateSomeData();
+
+            DisplayData();
+
+            Console.WriteLine("\nDeleting some data...");
+            DeleteSomeData();
+
+            DisplayData();
+        }
+        public static void DeleteSomeData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UpdateSomeData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void AddSomeDataFromUserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+            using var context = new ChinookContext(options);
+
+            var salespeople = context.Employee
+                .Where(e => e.Title.Contains("sales"))
+                .ToList();
+
+            foreach (var person in salespeople)
+            {
+                Console.WriteLine($"{person.Title} {person.FirstName} {person.LastName}");
+            }
         }
     }
 }
---
+++ sql-week-challenge.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Customers(
+	CustomersID INT IDENTITY(1, 1) NOT NULL,
+	FirstName NVARCHAR(200) NOT NULL,
+	LastName NVARCHAR(200) NOT NULL,
+	CardNumber INT NOT NULL, --before 'not null' one could place a check for number size 'CHECK (CardNumber >=99999999)'
+	CONSTRAINT PK_Customers PRIMARY KEY (CustomersID)
+);
+
+CREATE TABLE Products(
+	ProductsID INT IDENTITY(1, 1) NOT NULL,
+	Name NVARCHAR(200) NOT NULL,
+	Price NUMERIC(10,2) NOT NULL,
+	CONSTRAINT PK_ProductsID PRIMARY KEY (ProductsID)
+);
+
+CREATE TABLE Orders (
+	OrdersID INT NOT NULL,
+	ProductsID INT NOT NULL,
+	CustomersID INT NOT NULL,
+	CONSTRAINT PK_Orders PRIMARY KEY (CustomersID, ProductsID),
+	CONSTRAINT FK_Orders_Customers_CustomersID FOREIGN KEY (CustomersID)
+		REFERENCES Customers (CustomersID) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FK_Orders_Products_ProductsID FOREIGN KEY (ProductsID)
+		REFERENCES Products (ProductsID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO Products (Name, Price) VALUES 
+		('Tablet', 100),
+		('Radio', 45),
+		('Alarm Clock', 20);
+
+INSERT INTO Customers (FirstName, LastName, CardNumber) VALUES
+		('Daniel', 'Aasa', 88888888),
+		('Mike', 'Hale', 99999999),
+		('John', 'Smith', 12345678);
+
+INSERT INTO Orders (OrdersID, ProductsID, CustomersID) VALUES
+		(1, 1, 1),
+		(2, 2, 2),
+		(3, 3, 3);
+
+INSERT INTO Products (Name, Price) VALUES
+		('iPhone', 200);
+
+INSERT INTO Customers (FirstName, LastName, CardNumber) VALUES
+		('Tina', 'Smith', 0000000)
+
+INSERT INTO Orders (OrdersID, ProductsID, CustomersID) VALUES
+		(4, 4, 4);
+
+SELECT Customers.FirstName, Customers.LastName, Orders.OrdersID
+FROM Customers
+	left JOIN Orders ON Customers.CustomersID = Orders.CustomersID
+	--right JOIN Products ON Orders.ProductsID = Products.ProductsID;
+WHERE Customers.FirstName = 'Tina' AND Customers.LastName = 'Smith';
+
+SELECT SUM(Price) as iPhonesum
+FROM Products
+	FULL JOIN Orders ON Products.ProductsID = Orders.ProductsID
+WHERE Products.Name = 'iPhone';
