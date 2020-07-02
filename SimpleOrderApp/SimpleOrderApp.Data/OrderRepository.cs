1c9d9eab46dca9087d6af15fa94c8bf1c56dc549
Daniel Aasa <danielaasa42@gmail.com>
2020-07-02 10:25:56 -0700

    finished part four of the MVC Tutorial

---
+++ SimpleOrderApp/SimpleOrderApp.Data/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using SimpleOrderApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleOrderApp.Data
+{
+    class OrderRepository : IOrderRepository
+    {
+        private readonly SimpleOrderContext _context;
+
+        public OrderRepository(SimpleOrderContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Create(Order order)
+        {
+            var entities = _context.Orders
+        }
+
+
+        //Possible Error
+        // May need to use locationEntity instead of location, thus a need for mapping
+        public IEnumerable<Order> GetAll(Location location)
+        {
+            var entities = _context.Orders.ToList();
+
+            return entities.Select(e => new Order(e.Quantity, location, DateTime.Now));
+        }
+    }
+}
---
+++ SimpleOrderApp/SimpleOrderApp.Domain/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleOrderApp.Domain
+{
+    public interface IOrderRepository
+    {
+
+        //adding Location location for OrderRepository
+        IEnumerable<Order> GetAll(Location location);
+
+        void Create(Order order);
+    }
+}
